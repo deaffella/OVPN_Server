99e15a3edac8bf8c886d3a457ad12e8edd33042b
root <o1prime@yandex.ru>
2023-09-05 15:15:32 +0300

    ADD: - tg user whitelist;

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,4 +1,4 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update
 from telegram.ext import filters, \
     MessageHandler,\
     ApplicationBuilder, \
@@ -15,37 +15,50 @@ import requests
 from datetime import datetime
 import ipaddress
 
+import json
 from utils.pagination import OVPNUsersPaginator
 
 
 _secrets = dotenv_values("token.env.secret")
 TOKEN = _secrets['TOKEN']
+AUTHORIZED_TG_USERS = [str(usr['id']) for usr in json.load(open('./tg_users.json', 'r')).values()]
+
 API_URL = os.getenv('API_URL')
-OVPN_CONFIG_PATH = '/ovpn/openvpn.conf'
-OVPN_SUBNET = open(OVPN_CONFIG_PATH, 'r').readline().removeprefix('server ').split(' ')[0]
+OVPN_SUBNET = open('/ovpn/openvpn.conf', 'r').readline().removeprefix('server ').split(' ')[0]
 
 ovpn_users_paginator = OVPNUsersPaginator()
+
+
+def is_tg_user_in_whitelist(tg_id: str or int) -> bool:
+    return str(tg_id) in AUTHORIZED_TG_USERS
+
 
 
 async def command_start(update: Update, context: CallbackContext):
     user = update.effective_user
     user_name = user.username if user.username else user.first_name
 
-    text_to_reply = f'Привет, <b>{user_name}</b>!'
-    reply_markup = InlineKeyboardMarkup([
-        [InlineKeyboardButton('🌐 OVPN Пользователи', callback_data=f"{ovpn_users_paginator.prefix}:page=1")],
-        [InlineKeyboardButton('✉️ Telegram Пользователи', callback_data=f"telegram")],
-        [InlineKeyboardButton('🛈 Инфо', callback_data=f"info")]])
-    if hasattr(update, 'callback_query') and update.callback_query is not None:
-        query = update.callback_query
-        await query.answer()
-        await query.edit_message_text(text=text_to_reply, parse_mode=ParseMode.HTML)
-        await query.edit_message_reply_markup(reply_markup=reply_markup)
-    else:
-        await context.bot.send_message(text=text_to_reply,
+    if not is_tg_user_in_whitelist(tg_id=user.id):
+        print('asdasdas')
+        await context.bot.send_message(text='❌ Ты не в списке авторизованных пользователей! ❌ ',
                                        parse_mode=ParseMode.HTML,
-                                       chat_id=update.effective_chat.id,
-                                       reply_markup=reply_markup)
+                                       chat_id=update.effective_chat.id)
+    else:
+        text_to_reply = f'Привет, <b>{user_name}!</b>\n\n<i>tg_id: </i><code>{user.id}</code>'
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton('🌐 OVPN Пользователи', callback_data=f"{ovpn_users_paginator.prefix}:page=1")],
+            [InlineKeyboardButton('✉️ Telegram Пользователи', callback_data=f"telegram")],
+            [InlineKeyboardButton('🛈 Инфо', callback_data=f"info")]])
+        if hasattr(update, 'callback_query') and update.callback_query is not None:
+            query = update.callback_query
+            await query.answer()
+            await query.edit_message_text(text=text_to_reply, parse_mode=ParseMode.HTML)
+            await query.edit_message_reply_markup(reply_markup=reply_markup)
+        else:
+            await context.bot.send_message(text=text_to_reply,
+                                           parse_mode=ParseMode.HTML,
+                                           chat_id=update.effective_chat.id,
+                                           reply_markup=reply_markup)
 
 
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -58,29 +71,34 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         except:
             return False
 
-    text_from_user = str(update.message.text)
-    try:
-        user_name, ip = text_from_user.removeprefix('ovpn-add ').split(' ')[:2]
-        if not check_ip(ip):
-            text_to_reply = f'❌ Невозможно добавить пользователя <code>{user_name}</code>!\n\nОшибка:\n<code>{"Некорректный IP!"}</code>'
-        else:
-            new_ip = '.'.join(OVPN_SUBNET.split('.')[:-1] + [str(int(ip))])
-            result = requests.post(url=f'http://{API_URL}/add_user/', params={'name': user_name, 'ip': new_ip})
-            if result.status_code == 200:
-                text_to_reply = f'✅ Пользователь <code>{user_name}</code> добавлен!'
+
+
+    if not is_tg_user_in_whitelist(tg_id=update.message.from_user.id):
+        await context.bot.send_message(text='❌ Ты не в списке авторизованных пользователей! ❌ ',
+                                       parse_mode=ParseMode.HTML,
+                                       chat_id=update.effective_chat.id)
+    else:
+        text_from_user = str(update.message.text)
+        try:
+            user_name, ip = text_from_user.removeprefix('ovpn-add ').split(' ')[:2]
+            if not check_ip(ip):
+                text_to_reply = f'❌ Невозможно добавить пользователя <code>{user_name}</code>!\n\nОшибка:\n<code>{"Некорректный IP!"}</code>'
             else:
-                result_data = result.json()
-                text_to_reply = f'❌ Невозможно добавить пользователя <code>{user_name}</code>!\n\nОшибка:\n<code>{result_data["detail"]}</code>'
+                new_ip = '.'.join(OVPN_SUBNET.split('.')[:-1] + [str(int(ip))])
+                result = requests.post(url=f'http://{API_URL}/add_user/', params={'name': user_name, 'ip': new_ip})
+                if result.status_code == 200:
+                    text_to_reply = f'✅ Пользователь <code>{user_name}</code> добавлен!'
+                else:
+                    result_data = result.json()
+                    text_to_reply = f'❌ Невозможно добавить пользователя <code>{user_name}</code>!\n\nОшибка:\n<code>{result_data["detail"]}</code>'
 
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=text_to_reply, parse_mode=ParseMode.HTML,
-                                       reply_markup=InlineKeyboardMarkup([
-                                           [InlineKeyboardButton('⬅️ Назад', callback_data=f"{ovpn_users_paginator.prefix}:page=1"),
-                                            InlineKeyboardButton('🏠 Главное меню', callback_data=f"menu")]]))
-    except:
-        await show_all_ovpn_users(update=update, context=context)
-
-
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text=text_to_reply, parse_mode=ParseMode.HTML,
+                                           reply_markup=InlineKeyboardMarkup([
+                                               [InlineKeyboardButton('⬅️ Назад', callback_data=f"{ovpn_users_paginator.prefix}:page=1"),
+                                                InlineKeyboardButton('🏠 Главное меню', callback_data=f"menu")]]))
+        except:
+            await show_all_ovpn_users(update=update, context=context)
 
 
 async def show_all_ovpn_users(update: Update, context: CallbackContext):
@@ -149,8 +167,6 @@ async def delete_ovpn_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-
-
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
@@ -162,12 +178,5 @@ if __name__ == '__main__':
     application.add_handler(CallbackQueryHandler(show_ovpn_user_info, pattern=f'^{ovpn_users_paginator.prefix}:name=.*$'))
     application.add_handler(CallbackQueryHandler(download_ovpn_certificate, pattern=f'^{ovpn_users_paginator.prefix}:download=.*$'))
     application.add_handler(CallbackQueryHandler(delete_ovpn_user, pattern=f'^{ovpn_users_paginator.prefix}:delete=.*$'))
-
-    # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), text_handler)
-    # application.add_handler(echo_handler)
-
-    # application.add_handler(CommandHandler('start', command_start))
-    # application.add_handler(MessageHandler(filters.TEXT, handle_text))
-    #
 
     application.run_polling()
